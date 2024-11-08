a80990fa306e9adad6258af452b749a0712906a5
macmini <macmini@wylc.top>
2024-11-08 23:16:54 +0800

    perf: 重构媒体库刮削插件，增加判断文件入库日期并删除对应的nfo文件，以达到覆盖所有元数据和图片的效果

--- plugins.v2/libraryscraper/__init__.py
+++ plugins.v2/libraryscraper/__init__.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timedelta
 from pathlib import Path
 from threading import Event
@@ -19,21 +20,21 @@ from app.schemas import MediaType
 from app.utils.system import SystemUtils
 
 
-class LibraryScraper(_PluginBase):
+class LibraryScraperReconfiguration(_PluginBase):
     # 插件名称
-    plugin_name = "媒体库刮削"
+    plugin_name = "媒体库刮削-重构版"
     # 插件描述
     plugin_desc = "定时对媒体库进行刮削，补齐缺失元数据和图片。"
     # 插件图标
     plugin_icon = "scraper.png"
     # 插件版本
-    plugin_version = "2.0"
+    plugin_version = "1.0"
     # 插件作者
-    plugin_author = "jxxghp"
+    plugin_author = "wylc12138"
     # 作者主页
-    author_url = "https://github.com/jxxghp"
+    author_url = "https://github.com/wylc2138"
     # 插件配置项ID前缀
-    plugin_config_prefix = "libraryscraper_"
+    plugin_config_prefix = "libraryscraper_reconfiguration_"
     # 加载顺序
     plugin_order = 7
     # 可使用的用户级别
@@ -44,6 +45,8 @@ class LibraryScraper(_PluginBase):
     mediachain = None
     _scheduler = None
     _scraper = None
+    # 最近天数
+    _offset_days = "0"
     # 限速开关
     _enabled = False
     _onlyonce = False
@@ -64,6 +67,7 @@ class LibraryScraper(_PluginBase):
             self._mode = config.get("mode") or ""
             self._scraper_paths = config.get("scraper_paths") or ""
             self._exclude_paths = config.get("exclude_paths") or ""
+            self._offset_days = config.get("offset_days")
 
         # 停止现有任务
         self.stop_service()
@@ -86,7 +90,8 @@ class LibraryScraper(_PluginBase):
                     "cron": self._cron,
                     "mode": self._mode,
                     "scraper_paths": self._scraper_paths,
-                    "exclude_paths": self._exclude_paths
+                    "exclude_paths": self._exclude_paths,
+                    "offset_days": self._offset_days,
                 })
                 if self._scheduler.get_jobs():
                     # 启动服务
@@ -116,7 +121,7 @@ class LibraryScraper(_PluginBase):
         """
         if self._enabled and self._cron:
             return [{
-                "id": "LibraryScraper",
+                "id": "LibraryScraperReconfiguration",
                 "name": "媒体库刮削",
                 "trigger": CronTrigger.from_crontab(self._cron),
                 "func": self.__libraryscraper,
@@ -124,7 +129,7 @@ class LibraryScraper(_PluginBase):
             }]
         elif self._enabled:
             return [{
-                "id": "LibraryScraper",
+                "id": "LibraryScraperReconfiguration",
                 "name": "媒体库刮削",
                 "trigger": CronTrigger.from_crontab("0 0 */7 * *"),
                 "func": self.__libraryscraper,
@@ -181,7 +186,7 @@ class LibraryScraper(_PluginBase):
                                 'component': 'VCol',
                                 'props': {
                                     'cols': 12,
-                                    'md': 6
+                                    'md': 4
                                 },
                                 'content': [
                                     {
@@ -201,7 +206,7 @@ class LibraryScraper(_PluginBase):
                                 'component': 'VCol',
                                 'props': {
                                     'cols': 12,
-                                    'md': 6
+                                    'md': 4
                                 },
                                 'content': [
                                     {
@@ -213,7 +218,20 @@ class LibraryScraper(_PluginBase):
                                         }
                                     }
                                 ]
-                            }
+                            },
+                            {
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
+                                    {
+                                        "component": "VTextField",
+                                        "props": {
+                                            "model": "offset_days",
+                                            "label": "几天内",
+                                        },
+                                    }
+                                ],
+                            },
                         ]
                     },
                     {
@@ -443,6 +461,71 @@ class LibraryScraper(_PluginBase):
         except Exception as err:
             logger.warn(f"从nfo文件中获取tmdbid失败：{str(err)}")
         return None
+
+    @staticmethod
+    def __get_date(offset_day):
+        now_time = datetime.now()
+        end_time = now_time + timedelta(days=offset_day)
+        # 返回一个日期对象
+        return end_time.date()
+
+    @staticmethod
+    def get_all_subfolders(path: Path):
+        """获取指定路径下所有文件夹路径"""
+        subfolders = []
+
+        # 递归遍历文件夹
+        for root, dirs, files in os.walk(path):
+            for dir_name in dirs:
+                subfolder_path = os.path.join(root, dir_name)
+                subfolders.append(subfolder_path)
+
+        return subfolders
+
+    @staticmethod
+    def is_video_file(file_path):
+        # 常见视频文件扩展名
+        video_extensions = {'.mp4', '.mkv', '.flv', '.avi', '.mov', '.wmv', '.m4v', '.mpg', '.mpeg', '.3gp', '.webm'}
+
+        # 获取文件扩展名并转换为小写进行匹配
+        _, ext = os.path.splitext(file_path)
+        return ext.lower() in video_extensions
+
+    @staticmethod
+    def get_file_modification_date(file_path):
+        """获取文件的修改时间戳"""
+        timestamp = os.path.getmtime(file_path)
+        # 将时间戳转换为 datetime 对象
+        modification_datetime = datetime.fromtimestamp(timestamp)
+        # 返回一个 date 对象
+        return modification_datetime.date()
+
+    def browse_path(self, path: Path):
+        """遍历目录下的文件夹，获取视频文件入库时间，然后删除视频文件对应的nfo文件，强制覆盖元数据和图片"""
+        overwrite = True if self._mode else False
+        if not overwrite:  # 未开启覆盖所有元数据和图片，不删除nfo文件
+            return
+        # 获取指定天数之前的日期
+        end_date = self.__get_date(-int(self._offset_days))
+        subfolder_path = self.get_all_subfolders(path)
+        # 遍历所有文件夹下面的所有文件，并判断是否为视频文件且修改日期在指定天数内
+        for subfolder in subfolder_path:
+            for file in os.listdir(subfolder):
+                filepath = os.path.join(subfolder, file)
+                # 检查是否是文件（排除目录）
+                if os.path.isfile(filepath):
+                    if self.is_video_file(filepath):
+                        file_date = self.get_file_modification_date(filepath)
+                        if file_date >= end_date:
+                            # 获取文件的扩展名
+                            _, extension = os.path.splitext(filepath)
+                            # 获取文件名对应的nfo文件
+                            nfo_path = filepath.replace(extension, "nfo")
+                            logger.info(f"找到符合条件的nfo文件：{nfo_path}")
+                            # 删除文件名对应的nfo文件
+                            os.remove(nfo_path)
+
+
 
     def stop_service(self):
         """
