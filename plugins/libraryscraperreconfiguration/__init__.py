5313f47ecc8e439f5ff1421ab94fb0a7e36d25b4
macmini <macmini@wylc.top>
2024-11-09 10:00:44 +0800

    version: 版本更新

--- plugins/libraryscraperreconfiguration/__init__.py
+++ plugins/libraryscraperreconfiguration/__init__.py
@@ -26,7 +26,7 @@ class LibraryScraperReconfiguration(_PluginBase):
     # 插件图标
     plugin_icon = "scraper.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "wylc12138"
     # 作者主页
